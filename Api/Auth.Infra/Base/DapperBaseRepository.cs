a1fa65c80584b5476eae0f78a00c7e75bb9f565d
teixeiragthiago <kbeca-thiago@hotmail.com>
2020-09-19 14:33:01 -0300

    creating dapper repository to use SQL Server

---
+++ Api/Auth.Infra/Base/DapperBaseRepository.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Auth.Infra.Base
+{
+    public class DapperBaseRepository
+    {
+        private SqlConnection Create()
+        {
+            return new SqlConnection("");
+        }
+
+        public int Execute(string query)
+        {
+            int ret;
+            using (var cn = Create())
+            {
+                cn.Open();
+                ret = cn.Execute(query);
+                cn.Close();
+                cn.Dispose();
+            }
+
+            return ret;
+        }
+
+        public IEnumerable<T> Find<T>(string query)
+        {
+            IEnumerable<T> items;
+            using (var cn = Create())
+            {
+                cn.Open();
+                items = cn.Query<T>(query);
+                cn.Close();
+                cn.Dispose();
+            }
+
+            return items;
+        }
+    }
+}
